0030549b72578053487487016f236e9dcf5d94ac
유상욱 <tkddnr032@gmail.com>
2021-08-24 21:31:36 +0900

    VideoRPC

--- Assets/02.Scripts/UI/Main/MainVideoControl.cs
+++ Assets/02.Scripts/UI/Main/MainVideoControl.cs
@@ -3,47 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using Photon.Pun;
 
-public class MainVideoControl : MonoBehaviour
+public class MainVideoControl : MonoBehaviourPun
 {
     public GameObject PassWord;
     public GameObject VideoPanel;
     public GameObject Fail;
     public GameObject VideoPlayer;
     public InputField PwdText;
-    public VideoClip Team1Video;
-    public VideoClip Team2Video;
-    public VideoClip Team3Video;
-    public VideoClip Team4Video;
-    public VideoClip Team5Video;
-    public VideoClip Team6Video;
-    public VideoClip Team7Video;
-    public VideoClip Team8Video;
-    public VideoClip Team9Video;
-    public VideoClip Team10Video;
-    public VideoClip Team11Video;
-    public VideoClip Team12Video;
-    public VideoClip Team13Video;
-    public VideoClip Team14Vide;
-    public VideoClip Team15Vide;
-    public VideoClip Team16Video;
-    public VideoClip Team17Video;
-    public VideoClip Team18Video;
-    public VideoClip Team19Video;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public VideoClip[] TeamVideo = new VideoClip[19];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnClickPwdButton() //공지(교수용) 버튼 클릭
+    public void OnClickPwdButton()
     {
         if (PwdText.text == "0000") //비밀번호 0000
         {
@@ -51,20 +23,131 @@ public class MainVideoControl : MonoBehaviour
             VideoPanel.SetActive(true);
         }
         else
-        {
-            
+        {            
             Fail.SetActive(true);
         }
     }
+
+    #region 버튼
     public void Team1()
     {
-        VideoPlayer.GetComponent<VideoPlayer>().clip = Team1Video;
+        photonView.RPC("VideoRPC", RpcTarget.All, 0);
         VideoPanel.SetActive(false);
     }
 
     public void Team2()
     {
-        VideoPlayer.GetComponent<VideoPlayer>().clip = Team2Video;
+        photonView.RPC("VideoRPC", RpcTarget.All, 1);
         VideoPanel.SetActive(false);
+    }
+
+    public void Team3()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 2);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team4()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 3);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team5()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 4);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team6()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 5);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team7()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 6);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team8()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 7);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team9()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 8);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team10()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 9);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team11()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 10);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team12()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 11);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team13()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 12);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team14()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 13);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team15()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 14);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team16()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 15);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team17()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 16);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team18()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 17);
+        VideoPanel.SetActive(false);
+    }
+
+    public void Team19()
+    {
+        photonView.RPC("VideoRPC", RpcTarget.All, 18);
+        VideoPanel.SetActive(false);
+    }
+    #endregion
+
+    [PunRPC]
+    public void VideoRPC(int num)
+    {
+        VideoPlayer.GetComponent<VideoPlayer>().clip = TeamVideo[num];
+        VideoPlayer.GetComponent<VideoPlayer>().Play();
     }
 }
---
+++ Assets/02.Scripts/UI/Main/PanelVideoControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class PanelVideoControl : MonoBehaviour
+{
+    public VideoPlayer video;
+
+    public void videoPlay()
+    {
+        video.Play();
+    }
+
+    public void videoStop()
+    {
+        video.Stop();
+    }
+
+    public void videoPause()
+    {
+        video.Pause();
+    }
+}
