762accf35ae490802e39d2e6252c043ef5f780fb
한솔 <dhfaks79@gmail.com>
2021-08-26 17:01:08 +0900

    미션오류 수정

--- Assets/02.Scripts/Main/MissionControl.cs
+++ Assets/02.Scripts/Main/MissionControl.cs
@@ -13,7 +13,12 @@ public class MissionControl : MonoBehaviour
     GameObject CaptureButton;
     GameObject SungkyulButton;
 
+    public GameObject Stamp1_;
+    public GameObject Stamp2_;
     public GameObject Stamp3_;
+
+    public GameObject CompleteText;
+    public GameObject ParticleMission;
 
     int count_;
 
@@ -33,6 +38,28 @@ public class MissionControl : MonoBehaviour
         }
     }
 
+    //미션 완료
+    //설명: 만약 세가지 색상 알파값 모두가 1이 되면 미션완료 축하
+    //player 안에 있는 파티클 오브젝트를 on 시켜서 터트리는 방식
+    void Update()
+    {
+        Color FirstColor = Stamp1_.GetComponent<Image>().color;
+        Color SecondColor = Stamp2_.GetComponent<Image>().color;
+        Color ThirdColor = Stamp3_.GetComponent<Image>().color;
+        if (FirstColor.a == 1 && SecondColor.a == 1 && ThirdColor.a == 1)
+        {
+            //GameObject ParticleMission = GameObject.FindGameObjectWithTag("Particle");
+            //GameObject Particle = GameObject.FindGameObjectWithTag("Player");
+            
+            CompleteText.SetActive(true); //축하메세지 on
+            Stamp1_.SetActive(false);
+            Stamp2_.SetActive(false);
+            Stamp3_.SetActive(false);
+            ParticleMission.SetActive(true);
+        }
+    }
+
+    #region 미션창 열기닫기
     //미션창 열기
     public void OpenMission()
     {
@@ -48,4 +75,5 @@ public class MissionControl : MonoBehaviour
         Opendow_.SetActive(true);
         Closedow_.SetActive(false);
     }
+    #endregion
 }
