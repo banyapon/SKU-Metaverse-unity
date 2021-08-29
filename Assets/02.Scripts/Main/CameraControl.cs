b302955331bdcfdcad5e7769fea61981fe5fbf18
유상욱 <tkddnr032@gmail.com>
2021-08-29 15:28:15 +0900

    팔로우카메라 위치 수정

--- Assets/02.Scripts/Main/CameraControl.cs
+++ Assets/02.Scripts/Main/CameraControl.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public float XrotateSpeed = 1.0f;
-    public float YrotateSpeed = 1.0f;
     public float zoomSpeed = 10.0f;
-
     public Camera mainCamera;
 
     void Update()
     {
         Zoom();
-        //Rotate();
     }
 
     private void Zoom()
@@ -22,19 +18,6 @@ public class CameraControl : MonoBehaviour
         if (distance != 0)
         {
             mainCamera.fieldOfView += distance;
-        }
-    }
-
-    private void Rotate()
-    {
-        if (Input.GetMouseButton(1))
-        {
-            Vector3 rot = transform.rotation.eulerAngles;
-            rot.y += Input.GetAxis("Mouse X") * XrotateSpeed;
-            //rot.x += -1 * Input.GetAxis("Mouse Y") * YrotateSpeed;
-            Quaternion q = Quaternion.Euler(rot);
-            q.z = 0;
-            transform.rotation = Quaternion.Slerp(transform.rotation, q, 2f);
         }
     }
 }
