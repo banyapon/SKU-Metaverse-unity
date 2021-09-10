9b0304286249dca66a5088713bb340ca22081951
한솔 <80498475+amozo00@users.noreply.github.com>
2021-09-11 00:38:39 +0900

    update 오류수정
    미션완료 효과 사용할지 미정

--- Assets/02.Scripts/Main/MissionControl.cs
+++ Assets/02.Scripts/Main/MissionControl.cs
@@ -9,22 +9,9 @@ public class MissionControl : MonoBehaviour
     //Stamp1,2,3 태그 버튼들을 누르면 그에 맞는 도장의 알파값이 255로 변하는 구조
     #endregion
 
-    GameObject VideoButton;
-    GameObject CaptureButton;
-    GameObject SungkyulButton;
-
-    public GameObject Stamp1_;
-    public GameObject Stamp2_;
     public GameObject Stamp3_;
 
-    public GameObject CompleteText;
-    public GameObject ParticleMission;
-
     int count_;
-
-    public GameObject MissionWindow_;
-    public GameObject Opendow_;
-    public GameObject Closedow_;
 
     //성결이 도장
     public void OnStamp_3()
@@ -37,43 +24,4 @@ public class MissionControl : MonoBehaviour
             Stamp3_.GetComponent<Image>().color = SColor;
         }
     }
-
-    //미션 완료
-    //설명: 만약 세가지 색상 알파값 모두가 1이 되면 미션완료 축하
-    //player 안에 있는 파티클 오브젝트를 on 시켜서 터트리는 방식
-    void Update()
-    {
-        Color FirstColor = Stamp1_.GetComponent<Image>().color;
-        Color SecondColor = Stamp2_.GetComponent<Image>().color;
-        Color ThirdColor = Stamp3_.GetComponent<Image>().color;
-        if (FirstColor.a == 1 && SecondColor.a == 1 && ThirdColor.a == 1)
-        {
-            //GameObject ParticleMission = GameObject.FindGameObjectWithTag("Particle");
-            //GameObject Particle = GameObject.FindGameObjectWithTag("Player");
-            
-            CompleteText.SetActive(true); //축하메세지 on
-            Stamp1_.SetActive(false);
-            Stamp2_.SetActive(false);
-            Stamp3_.SetActive(false);
-            ParticleMission.SetActive(true);
-        }
-    }
-
-    #region 미션창 열기닫기
-    //미션창 열기
-    public void OpenMission()
-    {
-        MissionWindow_.SetActive(true);
-        Opendow_.SetActive(false);
-        Closedow_.SetActive(true);
-    }
-
-    //미션창 닫기
-    public void CloseMission()
-    {
-        MissionWindow_.SetActive(false);
-        Opendow_.SetActive(true);
-        Closedow_.SetActive(false);
-    }
-    #endregion
 }
---
+++ Assets/02.Scripts/Main/TextAction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAction : MonoBehaviour
+{
+    //Text CompleteText;
+    //float time;
+
+    //private void Start()
+    //{
+    //    int a = gameObject.GetComponent<Text>().fontSize;
+    //    time += Time.deltaTime;
+    //    for(int i =1; i <= 20; i++)
+    //    {
+    //        a = i;
+    //        if (a == 50)
+    //        {
+    //            a = 30;
+    //        }
+    //    }
+    //}
+
+    public GameObject CompleteText;
+    float time = 0;
+    public float _size = 2;
+
+    public float _upSizeTime = 0.1f;
+
+    void Update()
+    {
+        if (time <= _upSizeTime)
+        {
+            transform.localScale = Vector3.one * (1 + _size * time);
+        }
+        else if (time <= _upSizeTime * 2)
+        {
+            transform.localScale = Vector3.one * (2 * _size * _upSizeTime + 1 - time * _size);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+        time += Time.deltaTime;
+    }
+
+    public void resetAnim()
+    {
+        time = 0;
+    }
+
+}
