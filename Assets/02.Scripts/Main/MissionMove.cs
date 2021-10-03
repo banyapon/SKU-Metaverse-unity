8e64554c7cedc6ab40ac58d29d06cdd4f9410063
한솔 <80498475+SolHaan@users.noreply.github.com>
2021-10-03 20:22:05 +0900

    UGUI 변경
    부드러운 모션 추가, open/close 차이 추가, mission 완료 모션 변경

---
+++ Assets/02.Scripts/Main/MissionMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/*MissionMove: 부드럽게 움직이는 스크립트*/
+public class MissionMove : MonoBehaviour
+{
+    public GameObject Main_; //부모 창 오브젝트
+    public float posx = 1410;
+    public float r_posx = 1819;
+    public GameObject openButton;
+    public GameObject closeButton;
+
+    public void Open()
+    {
+        Main_.transform.DOMoveX(posx, 0.7f);
+
+        openButton.SetActive(false);
+        closeButton.SetActive(true);
+    }
+
+    public void Close()
+    {
+        Main_.transform.DOMoveX(r_posx, 0.7f);
+
+        openButton.SetActive(true);
+        closeButton.SetActive(false);
+    }
+}
