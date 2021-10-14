44ee28558d26e7fd96680cde30864b3b4f6e1ade
유상욱 <tkddnr032@gmail.com>
2021-10-14 22:02:22 +0900

    Update PlayerMove.cs

--- Assets/02.Scripts/Network/PlayerMove.cs
+++ Assets/02.Scripts/Network/PlayerMove.cs
@@ -6,11 +6,11 @@ using Photon.Realtime;
 
 public class PlayerMove : MonoBehaviourPun, IPunObservable
 {
-    public Transform _camera;
+    //public Transform _camera;
 
     private Vector3 remotePos;
     private Quaternion remoteRot;
-    private Quaternion remoteCamRot;
+    //private Quaternion remoteCamRot;
 
     void Update()
     {
@@ -18,7 +18,7 @@ public class PlayerMove : MonoBehaviourPun, IPunObservable
         {
             transform.position = Vector3.Lerp(transform.position, remotePos, 10 * Time.deltaTime);
             transform.rotation = Quaternion.Lerp(transform.rotation, remoteRot, 10 * Time.deltaTime);
-            _camera.rotation = Quaternion.Lerp(_camera.rotation, remoteCamRot, 10 * Time.deltaTime);
+            //_camera.rotation = Quaternion.Lerp(_camera.rotation, remoteCamRot, 10 * Time.deltaTime);
 
             return;
         }
@@ -30,14 +30,14 @@ public class PlayerMove : MonoBehaviourPun, IPunObservable
         {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
-            stream.SendNext(_camera.rotation);
+            //stream.SendNext(_camera.rotation);
         }
 
         else
         {
             remotePos = (Vector3)stream.ReceiveNext();
             remoteRot = (Quaternion)stream.ReceiveNext();
-            remoteCamRot = (Quaternion)stream.ReceiveNext();
+            //remoteCamRot = (Quaternion)stream.ReceiveNext();
         }
     }
 }
