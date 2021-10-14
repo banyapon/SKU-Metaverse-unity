e09e11dd51b2f9dc0ef588434a5d188d634c8fba
유상욱 <tkddnr032@gmail.com>
2021-10-14 21:59:56 +0900

    팔로우캠 수정

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -8,6 +8,9 @@ public class FollowCam : MonoBehaviourPun
     Transform target;
     Transform my_trans;
 
+    float zoomSpeed = 10.0f;
+    Camera mainCamera;
+
     void Start()
     {
         my_trans = GetComponent<Transform>();
@@ -15,6 +18,8 @@ public class FollowCam : MonoBehaviourPun
         if (photonView.IsMine)
         {
             target = Camera.main.transform;
+            mainCamera = Camera.main;
+
             target.position = new Vector3(my_trans.position.x, my_trans.position.y + 7.5f, my_trans.position.z - 10.5f);
             target.rotation = Quaternion.Euler(5f, 0f, 0f);
             target.transform.parent = transform;
@@ -28,8 +33,15 @@ public class FollowCam : MonoBehaviourPun
             return;
         }
 
-        //target.position = new Vector3 (my_trans.position.x, my_trans.position.y + 7.5f, my_trans.position.z - 10.5f);
-        //target.rotation = Quaternion.Euler (5f, 0f, 0f);
-        //target.LookAt(my_trans);
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+        if (distance != 0)
+        {
+            mainCamera.fieldOfView += distance;
+        }
     }
 }
