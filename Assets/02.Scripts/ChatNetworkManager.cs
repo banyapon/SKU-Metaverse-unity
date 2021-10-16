01ffa4bf88edef341c497bd0ded15bfd5b9ce458
chanmi99 <chm991104@naver.com>
2021-10-17 07:59:24 +0900

    채팅추가

---
+++ Assets/02.Scripts/ChatNetworkManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class ChatNetworkManager : MonoBehaviourPunCallbacks
+{
+    
+
+    [Header("ChatPanel")]
+    public GameObject ChatPanel;
+    public Text[] ChatText;
+    public InputField ChatInput;
+
+    [Header("ETC")]
+    public Text StatusText;
+    public PhotonView PV;
+
+
+
+    void Update()
+    {
+        StatusText.text = PhotonNetwork.NetworkClientState.ToString();
+    }
+
+    public void ChatOpen()
+    {
+        ChatPanel.SetActive(true);
+    }
+
+    public void ChatClose()
+    {
+        ChatPanel.SetActive(false);
+    }
+
+    #region 채팅
+    public void Send()
+    {
+        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
+        ChatInput.text = "";
+    }
+
+    [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    void ChatRPC(string msg)
+    {
+        bool isInput = false;
+        for (int i = 0; i < ChatText.Length; i++)
+            if (ChatText[i].text == "")
+            {
+                isInput = true;
+                ChatText[i].text = msg;
+                break;
+            }
+        if (!isInput) // 꽉차면 한칸씩 위로 올림
+        {
+            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
+            ChatText[ChatText.Length - 1].text = msg;
+        }
+    }
+    #endregion
+}
