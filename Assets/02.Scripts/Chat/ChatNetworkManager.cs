14e37c6433e55a6432fe7e90a44b8da6743f9b00
유상욱 <tkddnr032@gmail.com>
2021-10-17 16:18:14 +0900

    채팅목록 수정

--- Assets/02.Scripts/Chat/ChatNetworkManager.cs
+++ Assets/02.Scripts/Chat/ChatNetworkManager.cs
@@ -11,8 +11,9 @@ public class ChatNetworkManager : MonoBehaviourPunCallbacks
     public Text[] ChatText;
     public InputField ChatInput;
 
+    public GameObject SendBtn;
+
     public GameObject mainInputField;
-    public InputField MainInputField;
 
     public PhotonView PV;
 
@@ -31,18 +32,22 @@ public class ChatNetworkManager : MonoBehaviourPunCallbacks
         if (mainInputField.GetComponent<InputField>().isFocused == true)
         {
             mainInputField.GetComponent<Image>().color = new Color(255, 255, 255, 1.0f);
+            ChatInput.placeholder.color = new Color(50, 50, 50, 0.5f);
+            SendBtn.SetActive(true);
         }
 
         else
         {
-            mainInputField.GetComponent<Image>().color = new Color(255, 255, 255, 0.6f);
+            mainInputField.GetComponent<Image>().color = new Color(255, 255, 255, 0.0f);
+            ChatInput.placeholder.color = new Color(50, 50, 50, 0.0f);
+            SendBtn.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Send();
-            MainInputField.ActivateInputField();
-            MainInputField.Select();
+            ChatInput.ActivateInputField();
+            ChatInput.Select();
         }
     }
 
