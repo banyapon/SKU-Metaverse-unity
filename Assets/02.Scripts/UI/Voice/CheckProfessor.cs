f9407e346efa80836c87385d4087219e482ffe6e
유상욱 <tkddnr032@gmail.com>
2021-10-17 17:06:17 +0900

    관리자 탭 변경

--- Assets/02.Scripts/UI/Voice/CheckProfessor.cs
+++ Assets/02.Scripts/UI/Voice/CheckProfessor.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CheckProfessor : MonoBehaviour
 {
     public GameObject micButton;
+    public GameObject controlButton;
 
     void Start()
     {
         if (FindObjectOfType<GameManager>().IsToggle1 == true)
         {
             micButton.SetActive(true);
+            controlButton.SetActive(true);
         }    
     }
 }
