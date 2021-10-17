0800192a56074d7d99a646748d12aca8245c5e9f
유상욱 <tkddnr032@gmail.com>
2021-10-17 14:33:05 +0900

    스크린샷 수정

---
+++ Assets/02.Scripts/InputFieldMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class InputFieldMove : MonoBehaviourPun
+{
+    public void KeyStop()
+    {
+        if (photonView.IsMine)
+        {
+            gameObject.GetComponent<TPCharacterController3D>().enabled = false;
+        }
+    }
+}
