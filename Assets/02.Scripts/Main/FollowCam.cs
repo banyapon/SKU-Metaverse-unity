02beaa0dbee3ac77d151848bdf1175571e737fb1
유상욱 <tkddnr032@gmail.com>
2021-10-18 22:48:39 +0900

    Update FollowCam.cs

--- Assets/02.Scripts/Main/FollowCam.cs
+++ Assets/02.Scripts/Main/FollowCam.cs
@@ -23,7 +23,7 @@ public class FollowCam : MonoBehaviourPun
             target = Camera.main.transform;
             mainCamera = Camera.main;
 
-            target.position = new Vector3(my_trans.position.x, my_trans.position.y + 7.5f, my_trans.position.z - 11.5f);
+            target.position = new Vector3(my_trans.position.x, my_trans.position.y + 7.5f, my_trans.position.z - 14.0f);
             target.rotation = Quaternion.Euler(5f, 0f, 0f);
             target.transform.parent = transform;
         }
