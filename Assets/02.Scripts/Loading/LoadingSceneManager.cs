e4b27ea1ecd5adc456afbb77fcb00fa1bb1e1f42
Yun Ji Yeon <woal0929@gmail.com>
2021-10-19 03:34:37 +0900

    비동기로드
    로딩 후 메인으로 씬 전환 완료

---
+++ Assets/02.Scripts/Loading/LoadingSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    public Slider slider;
+    public string SceneName;
+
+    private float time;
+
+
+    void Start()
+    {
+        StartCoroutine(LoadAsynSceneCoroutine());
+        
+    }
+
+    IEnumerator LoadAsynSceneCoroutine()
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneName);
+
+        operation.allowSceneActivation = false;
+
+
+
+        while (!operation.isDone)
+        {
+
+            slider.value = operation.progress;
+
+            operation.allowSceneActivation = true;
+
+            yield return null;
+        }
+
+    }
+}
