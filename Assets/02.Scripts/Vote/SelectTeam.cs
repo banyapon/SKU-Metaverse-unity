79e553b1dc0c7d7b8d93fa07bcd5f017fbdf5df0
유상욱 <tkddnr032@gmail.com>
2021-10-19 18:08:44 +0900

    Q&A 문구추가

---
+++ Assets/02.Scripts/Vote/SelectTeam.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTeam : MonoBehaviour
+{
+    public static string voteTeam;
+
+    public void Team1()
+    {
+        voteTeam = "Team1";
+    }
+
+    public void Team2()
+    {
+        voteTeam = "Team2";
+    }
+
+    public void Team3()
+    {
+        voteTeam = "Team3";
+    }
+
+    public void Team4()
+    {
+        voteTeam = "Team4";
+    }
+
+    public void Team5()
+    {
+        voteTeam = "Team5";
+    }
+
+    public void Team6()
+    {
+        voteTeam = "Team6";
+    }
+
+    public void Team7()
+    {
+        voteTeam = "Team7";
+    }
+
+    public void Team8()
+    {
+        voteTeam = "Team8";
+    }
+
+    public void Team9()
+    {
+        voteTeam = "Team9";
+    }
+
+    public void Team10()
+    {
+        voteTeam = "Team10";
+    }
+
+    public void Team11()
+    {
+        voteTeam = "Team11";
+    }
+
+    public void Team12()
+    {
+        voteTeam = "Team12";
+    }
+
+    public void Team13()
+    {
+        voteTeam = "Team13";
+    }
+
+    public void Team14()
+    {
+        voteTeam = "Team14";
+    }
+
+    public void Team15()
+    {
+        voteTeam = "Team15";
+    }
+
+    public void Team16()
+    {
+        voteTeam = "Team16";
+    }
+
+    public void Team17()
+    {
+        voteTeam = "Team17";
+    }
+
+    public void Team18()
+    {
+        voteTeam = "Team18";
+    }
+}
